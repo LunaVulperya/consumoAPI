935b72035169f9fb49f4fcf4c69f9e2c7dc74dd8
LunaVulperya <lunavulperya@gmail.com>
2025-05-27 20:27:10 -0300

    Added a CPF Mask to the site via JS

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+let cpf = document.querySelector("#cpf");
+
+cpf.addEventListener("keypress", ()=>{
+    let tamanhoCampo = cpf.value.length;
+    if(tamanhoCampo == 3 || tamanhoCampo == 7){
+        cpf.value += ".";
+    } else if(tamanhoCampo == 11){
+        cpf.value += "-";
+    }
+});
+
+//Impede caracteres alem de numeros de serem digitados
+function somenteNumeros(e) {
+    var charCode = e.charCode ? e.charCode : e.keyCode;
+    // charCode 8 = backspace   
+    // charCode 9 = tab
+    if (charCode != 8 && charCode != 9) {
+        // charCode 48 equivale a 0   
+        // charCode 57 equivale a 9
+        if (charCode < 48 || charCode > 57) {
+            return false;
+        }
+    }
+}
+
 function limpa_formulário_cep() {
     //Limpa valores do formulário de cep.
     document.getElementById('rua').value=("");
